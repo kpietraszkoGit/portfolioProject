2aaaed8b71d19a860ee3257187ca2dd58f64413e
architektmazgaj <73141073+architektmazgaj@users.noreply.github.com>
2021-05-18 22:20:22 +0200

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h2>Portfolio Project...</h2>
+    <Navbar />
   );
 }
 
