d94e38bbdf3691ae4cd5dada89031b37eb3c89ac
architektmazgaj <73141073+architektmazgaj@users.noreply.github.com>
2021-05-24 17:48:21 +0200

    Testimonials Component Completed cd

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -109,26 +109,26 @@ const Pofrfolio = () => {
     <div id="portfolio" className="portfolio-wrapper">
       <div className="container">
         <h1 className="text-uppercase text-center py-5">portfolio</h1>
-        <div className="image-box-wrapper row justify-content-center">
-          <div className="portfolio-image-box" onClick={openPopupboxNetflix}>
+        <div className="image-box-wrapper row">
+          <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxNetflix}>
             <img className="portfolio-image" src={netflix} alt="Netflix Clone Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="portfolio-image-box" onClick={openPopupboxCityGuide}>
+          <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxCityGuide}>
             <img className="portfolio-image" src={cityGuide} alt="City Guide Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="portfolio-image-box" onClick={openPopupboxPortfolio}>
+          <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxPortfolio}>
             <img className="portfolio-image" src={portfolio} alt="Portfolio React and Material UI Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="portfolio-image-box" onClick={openPopupboxTaskManager}>
+          <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxTaskManager}>
             <img className="portfolio-image" src={taskManager} alt="Task Manager React and Redux Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
