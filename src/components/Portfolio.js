782afad4e7a326f4f9f4d8bb880d353d171e8718
architektmazgaj <73141073+architektmazgaj@users.noreply.github.com>
2021-06-24 20:01:46 +0200

    icons  completed

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,10 @@
 import React from "react";
-import netflix from "../images/netflix.png";
-// import cityGuide from "../images/city-guide-app.png";
-// import portfolio from "../images/portfolio.png";
-// import taskManager from "../images/task-manager.png";
-import finance2a from "../finance2a.jpg";
+
+import wealth from "../images/wealth.png";
+import invest from "../images/invest.png";
+import cash from "../images/cash.png";
+import money from "../images/money.png";
+
 // FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
@@ -19,7 +20,7 @@ const Pofrfolio = () => {
   const openPopupboxNetflix = () => {
     const content = (
       <>
-        <img className="portfolio-image-popupbox" src={netflix} alt="Netflix Clone Project..." />
+        <img className="portfolio-image-popupbox" src={wealth} alt="Netflix Clone Project..." />
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
         <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/8020Coding/netflix-projects")}>https://</a>
       </>
@@ -40,7 +41,7 @@ const Pofrfolio = () => {
   const openPopupboxCityGuide = () => {
     const content = (
       <>
-        <img className="portfolio-image-popupbox" src={finance2a} alt="City Guide App Project..." />
+        <img className="portfolio-image-popupbox" src={invest} alt="City Guide App Project..." />
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
         <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://city-guide-app-project.herokuapp.coms/", "_blank")}>https://</a>
         <br />
@@ -63,7 +64,7 @@ const Pofrfolio = () => {
   const openPopupboxPortfolio = () => {
     const content = (
       <>
-        <img className="portfolio-image-popupbox" src={finance2a} alt="Portfolio Project..." />
+        <img className="portfolio-image-popupbox" src={cash} alt="Portfolio Project..." />
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
         <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://portfolio-rea-and-material-ui.herokuapp.coms/", "_blank")}>https://</a>
         <br />
@@ -86,7 +87,7 @@ const Pofrfolio = () => {
   const openPopupboxTaskManager = () => {
     const content = (
       <>
-        <img className="portfolio-image-popupbox" src={finance2a} alt="Task Manager React and Redux Project..." />
+        <img className="portfolio-image-popupbox" src={money} alt="Task Manager React and Redux Project..." />
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
         <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://react-redux-task-manager.herokuapp.com/a", "_blank")}>https://</a>
         <br />
@@ -110,29 +111,29 @@ const Pofrfolio = () => {
     <div id="portfolio" className="portfolio-wrapper">
       <div className="container">
         <h1 className="text-uppercase text-center py-5">klienci biznesowi</h1>
-        <div className="text-uppercase text-center text-portflio">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis. 
+        <div className="text-center text-portflio">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis. 
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</div>
         <div className="image-box-wrapper row">
           <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxNetflix}>
-            <img className="portfolio-image" src={netflix} alt="Netflix Clone Project..." />
+            <img className="portfolio-image" src={wealth} alt="Netflix Clone Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
           <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxCityGuide}>
-            <img className="portfolio-image" src={finance2a} alt="City Guide Project..." />
+            <img className="portfolio-image" src={invest} alt="City Guide Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
           <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxPortfolio}>
-            <img className="portfolio-image" src={finance2a} alt="Portfolio React and Material UI Project..." />
+            <img className="portfolio-image" src={cash} alt="Portfolio React and Material UI Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
           <div className="portfolio-image-box col-md-6 col-lg-3" onClick={openPopupboxTaskManager}>
-            <img className="portfolio-image" src={finance2a} alt="Task Manager React and Redux Project..." />
+            <img className="portfolio-image" src={money} alt="Task Manager React and Redux Project..." />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -5,6 +5,9 @@ import { faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
 import { IoPeopleOutline, IoCreateOutline } from 'react-icons/io5';
 import { IoIosSearch } from 'react-icons/io';
 import ContactBox from "./ContactBox";
+import location from "../images/icons/location.png";
+import search from "../images/icons/search.png";
+import write from "../images/icons/write.png";
 
 const Services = () => {
   return (
@@ -13,7 +16,7 @@ const Services = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-6 col-md-6 col-sm-6 px-5">
-            <div className="line">____________________</div>
+            <div className="line">___________________</div>
             <p>Lucas ipsum dolor sit amet alderaan hutt mustafar aayla ackbar anakin secura antilles solo windu. Hoth kenobi leia kit anakin jar darth.
             Lucas ipsum dolor sit amet alderaan hutt mustafar aayla ackbar anakin secura antilles solo windu. Hoth kenobi leia kit anakin jar darth.
             Lucas ipsum dolor sit amet alderaan hutt mustafar aayla ackbar anakin secura antilles solo windu. Hoth kenobi leia kit anakin jar darth.
@@ -41,17 +44,26 @@ const Services = () => {
           <div className="col-lg-6 col-md-6 col-sm-6 px-5">
             <ul className="work-points">1. Spotkanie i Analiza Finansowa</ul>
             <div className="row">
-              <IoPeopleOutline className="col-lg-2 col-md-2 col-sm-1 work-icons"/>
+              <div className="col-lg-2 col-md-2 col-sm-1 text-center">
+                <img className="work-icons img-fluid" src={location} alt="location..." />
+              </div>
+              {/* <IoPeopleOutline className="col-lg-2 col-md-2 col-sm-1 work-icons"/> */}
               <p className="col-lg-10 col-md-10 col-sm-11">Lucas ipsum dolor sit amet alderaan hutt mustafar aayla ackbar anakin secura antilles solo windu. Hoth kenobi leia kit anakin jar darth.</p>
             </div>
             <ul className="work-points">2. Wyszukanie, przygotowanie i konsultacja rozwiązań</ul>
             <div className="row">
-              <IoIosSearch className="col-lg-2 col-md-2 col-sm-1 work-icons"/>
+              <div className="col-lg-2 col-md-2 col-sm-1 text-center">
+                <img className="work-icons img-fluid" src={search} alt="search..." />
+              </div>
+              {/* <IoIosSearch className="col-lg-2 col-md-2 col-sm-1 work-icons"/> */}
               <p className="col-lg-10 col-md-10 col-sm-11">Lucas ipsum dolor sit amet alderaan hutt mustafar aayla ackbar anakin secura antilles solo windu. Hoth kenobi leia kit anakin jar darth.</p>
             </div>
             <ul className="work-points">3. Podpisanie umowy</ul>
             <div className="row">
-              <IoCreateOutline className="col-lg-2 col-md-2 col-sm-1 work-icons"/>
+              <div className="col-lg-2 col-md-2 col-sm-1 text-center">
+                <img className="work-icons img-fluid" src={write} alt="write..." />
+              </div>
+              {/* <IoCreateOutline className="col-lg-2 col-md-2 col-sm-1 work-icons"/> */}
               <p className="col-lg-10 col-md-10 col-sm-11">Lucas ipsum dolor sit amet alderaan hutt mustafar aayla ackbar anakin secura antilles solo windu. Hoth kenobi leia kit anakin jar darth.</p>
             </div>
           </div>
